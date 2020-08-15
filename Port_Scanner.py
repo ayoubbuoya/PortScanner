91b575aa49ed26fa060525c902778630f3586a2c
ayoubbuoya <69482481+ayoubbuoya@users.noreply.github.com>
2020-08-15 19:51:11 +0100

    Add files via upload

---
+++ Port_Scanner.py
@@ -0,0 +1,51 @@
+#importing
+import socket
+from datetime import datetime
+import os
+import subprocess
+#Clear secreen
+subprocess.call('clear',shell=True)
+#data inputs
+Server_host=socket.gethostname()
+Server_Ip=socket.gethostbyname(Server_host)
+#beuatifull banner 
+print("-"*84)
+print("")
+print("      Scanning Remote Ip : "+Server_Ip)
+print("")
+print("-"*84)
+#check what time the scan started
+t1=datetime.now()
+#Scan Ports with printing the Error
+try :
+    for port in range(1,1025) :
+        sock=socket.socket()
+        result=sock.connect_ex((Server_Ip,port))
+        #this fuction will return 0 if the port is open
+        if result== 0 :
+            print("port{} ===> Is Open".format(port))
+        sock.close()
+except KeyboardInterrupt :
+        print("Exiting ..............")
+        sys.exit()
+except socket.gaierror :
+        print("Socket Name Can not resolved ")
+        print("")
+        print("Exiting ..............") 
+        sys.exit()
+except socket.error :
+        print("Can't Connet To Server ")
+        sys.exit()
+#Check time again
+t2=datetime.now()
+#total time of running this script
+time=t2-t1
+print(" [ "+" AyoubBuoya "*3+" ]  ")
+print("")
+print("    Script Run For ===> {} S".format(time))
+print("")
+print(" [ "+" AyoubBuoya "*3+" ]  ")
+
+        
+        
+        
